90ea8eafe33acd06d56cea27bd8dee25648fe199
chenviktoriia <torichvic2@gmail.com>
2026-05-27 10:45:09 +0900

    task-11

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-// Реальная задача 10: цена с налогом
+// Реальная задача 11: функция использует другую функцию
+// Теперь напиши функцию calculateFinalOrderPrice(productPrice, deliveryPrice, taxPercent), которая:
 //
-// Напиши функцию calculatePriceWithTax(price, taxPercent), которая возвращает цену с налогом.
-
+// считает цену товара с налогом через calculatePriceWithTax;
+// добавляет доставку;
+// если taxPercent не передали, должен использоваться стандартный налог 20.
 // Примеры:
-// calculatePriceWithTax(1000);     // 1200
-// calculatePriceWithTax(500, 0);   // 500
+// calculateFinalOrderPrice(1000, 300);     // 1500
+// calculateFinalOrderPrice(500, 100, 0);   // 600
+
 
 function calculatePriceWithTax(price, taxPercent = 20) {
     return price + price * taxPercent / 100;
+}
+
+function calculateFinalOrderPrice(productPrice, deliveryPrice, taxPercent) {
+    return calculatePriceWithTax(productPrice, taxPercent) + deliveryPrice;
 }
