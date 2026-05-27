528b215e49d81dc60896f0fbe5b56fd358863569
chenviktoriia <torichvic2@gmail.com>
2026-05-27 10:58:36 +0900

    task-12

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-// Реальная задача 11: функция использует другую функцию
-// Теперь напиши функцию calculateFinalOrderPrice(productPrice, deliveryPrice, taxPercent), которая:
+// Контрольная задача перед массивами
 //
-// считает цену товара с налогом через calculatePriceWithTax;
-// добавляет доставку;
-// если taxPercent не передали, должен использоваться стандартный налог 20.
+// Напиши функцию getCheckoutMessage(productPrice, deliveryPrice, isPaid, taxPercent), которая возвращает сообщение для оформления заказа.
+//
+//     Правила:
+//
+// если isPaid === false, вернуть "Заказ ожидает оплаты"
+// если заказ оплачен:
+//     посчитать итоговую сумму: товар с налогом + доставка;
+// если taxPercent не передали, использовать налог 20;
+// вернуть строку "Заказ оплачен. Итого: X"
 // Примеры:
-// calculateFinalOrderPrice(1000, 300);     // 1500
-// calculateFinalOrderPrice(500, 100, 0);   // 600
+//
+//     getCheckoutMessage(1000, 300, false, 10); // "Заказ ожидает оплаты"
+// getCheckoutMessage(1000, 300, true, 10);  // "Заказ оплачен. Итого: 1400"
+// getCheckoutMessage(1000, 300, true);      // "Заказ оплачен. Итого: 1500"
 
+function getCheckoutMessage(productPrice, deliveryPrice, isPaid, taxPercent = 20) {
+    if (!isPaid) {
+        return "Заказ ожидает оплаты";
+    }
 
-function calculatePriceWithTax(price, taxPercent = 20) {
-    return price + price * taxPercent / 100;
+    const orderTotalPrice = productPrice + deliveryPrice + calculateTaxAmount(productPrice, taxPercent);
+    return `Заказ оплачен. Итого: ${orderTotalPrice}`;
 }
 
-function calculateFinalOrderPrice(productPrice, deliveryPrice, taxPercent) {
-    return calculatePriceWithTax(productPrice, taxPercent) + deliveryPrice;
+function calculateTaxAmount(productPrice, taxPercent) {
+    return productPrice * taxPercent / 100;
 }
